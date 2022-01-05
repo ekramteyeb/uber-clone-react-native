4d0cd8358defd79099811437980a2c731137fa45
Teyeb <hussenab@gmail.com>
2022-01-06 01:49:57 +0200

    uber-clone initialized

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import HomeScreen from "./screens/HomeScreen";
+import { store } from "./store";
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Text>I am the HomeScreen</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
