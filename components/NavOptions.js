2ec59e9868c402b8a4654d051b0d41d3b735a9eb
Teyeb <hussenab@gmail.com>
2022-01-06 20:56:50 +0200

    HomeScreen , navOptions added

---
+++ components/NavOptions.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { FlatList, TouchableOpacity, Text, View, Image } from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    id: "1234",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    sreen: "MapScreen",
+  },
+  {
+    id: "5678",
+    title: "Order Food",
+    image: "https://links.papareact.com/28w",
+    sreen: "EatsScreen",
+  },
+];
+const NavOptions = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name="arrowright"
+              color="white"
+              type="antdesign"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>I am the HomeScreen</Text>
+    <SafeAreaView style={[tw`bg-white h-full`]}>
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+        <NavOptions />
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    color: "blue",
+  },
+});
