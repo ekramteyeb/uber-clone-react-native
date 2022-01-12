4bdadabfc1b7db0bc404b002b5a9feade3ae1497
Teyeb <hussenab@gmail.com>
2022-01-13 01:28:05 +0200

    changed added

---
+++ screens/EatsScreen.js
@@ -0,0 +1,74 @@
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
+import {
+  SafeAreaView,
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  SectionList,
+  StyleSheet,
+} from "react-native";
+import { Icon } from "react-native-elements";
+import tw from "tailwind-react-native-classnames";
+
+const data = [
+  {
+    title: "Main dishes",
+    data: ["Pizza", "Burger", "Risotto"],
+  },
+  {
+    title: "Sides",
+    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
+  },
+  {
+    title: "Drinks",
+    data: ["Water", "Coke", "Beer"],
+  },
+  {
+    title: "Desserts",
+    data: ["Cheese Cake", "Ice Cream"],
+  },
+];
+const Item = ({ title }) => (
+  <View style={[styles.item, tw`bg-gray-200`]}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
+const EatsScreen = () => {
+  return (
+    <SafeAreaView style={[styles.container, tw`bg-white h-full `]}>
+      <SectionList
+        style={tw`pt-2`}
+        sections={data}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => <Item title={item} />}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={[styles.header, tw`bg-red-200 `]}>{title}</Text>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 16,
+  },
+  item: {
+    padding: 20,
+    marginVertical: 8,
+  },
+  header: {
+    fontSize: 32,
+    backgroundColor: "#ffff",
+  },
+  title: {
+    fontSize: 24,
+  },
+});
+
+export default EatsScreen;
