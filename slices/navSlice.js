73b4014de50ece85155b7fdd13a1f2d771669d49
Teyeb <hussenab@gmail.com>
2022-02-01 21:47:04 +0200

    navslice

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -27,9 +27,9 @@ export const { setOrigin, setDestination, setTravelTimeInformation } =
 
 //Selectors
 
-export const selectOrigin = (state) => state.navSlice.origin;
-export const selectDestination = (state) => state.navSlice.destination;
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
 export const selectTravelTimeInformation = (state) =>
-  state.navSlice.travelTimeInformation;
+  state.nav.travelTimeInformation;
 
 export default navSlice.reducer;
